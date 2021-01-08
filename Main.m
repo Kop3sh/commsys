2fb431f57b67f61c65431264f65b80e4f85c7c6c
ahelfouly <75209455+ahelfouly@users.noreply.github.com>
2021-01-08 15:03:13 +0200

    Fixed channel error

--- Main.m
+++ Main.m
@@ -3,8 +3,8 @@ L = length(sig_t);
 sound(sig_t,fs);
 
 %plot in time domain 
-t = linspace(0,length(sig_t)/fs,length(sig_t));
-figure; plot(t,sig_t); title('Original Signal in Time');
+tvec = linspace(0,length(sig_t)/fs,length(sig_t));
+figure; plot(tvec,sig_t); title('Original Signal in Time');
 
 %plot in frequency domain
 sig_f = abs(fftshift(fft(sig_t)));
@@ -12,22 +12,28 @@ fvec = linspace(-fs/2,fs/2,length(sig_f));
 figure; plot(fvec,sig_f); title('Original Signal in Frequency');
 
 %channel
+
 nh = input('\n\nChoose the number of the impluse response\n 1.Delta\n 2. exp(-2pi*5000t)\n 3. exp(-2pi*1000t)\n 4. 2*delta(t)+0.5*delta(t-1)\n');
 switch nh
     case 1
-        h = [1 zeros(1,length(sig_t))];
+        h = [1 zeros(1,L/2)];
     case 2
         h = exp(-2*pi*5000*tvec);
     case 3
         h = exp(-2*pi*1000*tvec);
     case 4
         h = [2 zeros(1,fs-1) 0.5];
-        h = [h zeros(1, length(sig_t)-length(h))];
+        h = [h zeros(1, (L/2)-length(h))];
 end
-sig_t = conv(sig_t,h);
+s1 = sig_t(:,1).';
+s1 = conv(s1,h);
+s2 = sig_t(:,2).';
+s2 = conv(s2,h);
+
+sig_t = [s1;s2].'
 
 tvec = linspace(0,length(sig_t)/fs,length(sig_t));
-figure; plot(t,sig_t); title('Convoluted Signal in Time');
+figure; plot(tvec,sig_t); title('Convoluted Signal in Time');
 sig_f = abs(fftshift(fft(sig_t)));
 fvec = linspace(-fs/2,fs/2,length(sig_f));
 figure; plot(fvec,sig_f); title('Convoluted Signal in Frequency');
